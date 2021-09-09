581615a8bc583a90e42139ad8ef5cb32db8f7622
Зося Довганич <90170374+ZosiaD@users.noreply.github.com>
2021-09-09 20:39:54 +0300

    Add files via upload

---
+++ task9.py
@@ -0,0 +1,6 @@
+"""Write a program that asks the user to enter an angle
+between -180° and 180°. Using an expression with the modulo
+operator,convert the angle to its equivalent between 0° and 360°"""
+
+angle = int(input("Angle = "))
+print("Equivalent between 0° and 360°", angle + 180)
